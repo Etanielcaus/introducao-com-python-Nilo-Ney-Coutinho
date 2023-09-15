04984b57915ab9fc69916f8c02752e5a830e7e41
Etanielcaus <nielfilosofia@gmail.com>
2023-09-15 07:03:54 -0300

    atualização

---
+++ capitulo-2-preparando-ambiente/2_4_cuidados_ao_digitar_seus_programas.py
@@ -0,0 +1,6 @@
+# Cuidados ao digitar programas
+# 1 - Letras maiúsculas e minúsculas são diferentes: print é diferente de Print
+# 2 - Aspas são importantes e não devem ser esquecidas.
+# 3 - Parênteses não são opicionais em Python.
+# 4 - Espaços são muito importantes em Python. Deve se levar em consideração
+# que TAB adiciona 4 espaços, e não deve ser maior que isso.
